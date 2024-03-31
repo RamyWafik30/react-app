db02adf21b9012c199b2a4807b3510cb12c9eb88
Your Name <you@example.com>
2024-03-31 23:32:08 +0200

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import Header from './components/common/heading/Header';
+import { BrowserRouter as Router , Switch , Route  } from 'react-router-dom';
 
-function App() {
+import Home from './components/home/Home';
+import About from './components/about/About';
+import CourseHome from './components/allcourses/CourseHome';
+import Team from './components/taem/Team';
+
+
+import Price from './components/pricing/Price';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
+import Footer from './components/common/footer/Footer';
+
+
+// import { BrowserRouter as Router , Switch , Route , Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+function App  ()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+       <Header />
+       <Switch>
+       <Route path="/"  exact component={Home} />
+       <Route path="/about"  component={About} />
+       <Route path="/courses"  component={CourseHome} />
+       <Route path="/team"  component={Team} /> 
+       <Route path="/pricing"  component={Price} /> 
+       <Route path="/journal"  component={Blog} /> 
+       <Route path="/contact"  component={Contact} /> 
+
+
+
+       </Switch>\
+       <Footer />
+    </Router>
+     
+    </>
+  )
 }
 
 export default App;
